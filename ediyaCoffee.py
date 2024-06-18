77f09e31e3f7a9480b552865cc5da3542650178e
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-06-19 00:52:16 +0900

    Update ediyaCoffee.py

--- ediyaCoffee.py
+++ ediyaCoffee.py
@@ -1,24 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from datetime import datetime
 import time
 import json
-import os
 
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
-folder_path = "ediya"
-filename = f"{folder_path}/menuediya_{current_date}.json"
-
-# 폴더 생성
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
+filename = f"menu-ediya_{current_date}.json"
 
 # 웹드라이버 초기화 (Chrome 사용)
 options = ChromeOptions()
@@ -54,13 +48,14 @@ tracks = soup.select("#menu_ul li")
 for track in tracks:
     name = track.select_one(".menu_tt > a > span").text.strip()
     image_url = track.select_one("a > img").get('src').replace('/images', 'https://www.ediya.com/files')
+    
     detail_con = track.select_one(".detail_con")
     
     titleE = detail_con.select_one("h2 > span").text.strip() if detail_con.select_one("h2 > span") else ""
     desction = detail_con.select_one("p").text.strip() if detail_con.select_one("p") else ""
 
     nutrition_info = {}
-    tbody = detail_con.select_one(".pro_nutri")
+    tbody = detail_con.select_one(".pro_nutri") if detail_con else None
     if tbody:
         rows = tbody.find_all("dl")
         for row in rows:
